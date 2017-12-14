376fe2dff4b2fcef726aff05e818fd08ded601c7
Sebastian Hönel <sehoaa@lnu.se>
2017-12-14 17:37:07 +0100

    Extend Util.Configuration to support databases

--- GitDensity/Util/Configuration.cs
+++ GitDensity/Util/Configuration.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using GitDensity.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,8 @@ namespace GitDensity.Util
 {
 	public class Configuration
 	{
+		public const String DefaultFileName = "configuration.json";
+
 		/// <summary>
 		/// The absolute path to the executable for handling clone detection.
 		/// This should not contain any arguments, use the other property
@@ -27,13 +31,32 @@ namespace GitDensity.Util
 		public String CloneDetectionArgs { get; set; }
 
 		/// <summary>
+		/// Specify the database to use. This application can operate with an
+		/// in-memory database as well.
+		/// </summary>
+		[JsonProperty(Required = Required.Always, PropertyName = "databaseType")]
+		[JsonConverter(typeof(StringEnumConverter))]
+		public DatabaseType DatabaseType { get; set; } = DatabaseType.SQLiteTemp;
+
+		/// <summary>
+		/// A nullable string that contains all necessary details to establish a
+		/// connection to the selected database. If the selected <see cref="DatabaseType"/>
+		/// is equal to <see cref="DatabaseType.SQLiteTemp"/>, then this string may
+		/// be null or empty.
+		/// </summary>
+		[JsonProperty(Required = Required.AllowNull, PropertyName = "databaseConnectionString")]
+		public String DatabaseConnectionString { get; set; } = null;
+
+		/// <summary>
 		/// An example that is used to create an initial configuration, if
 		/// none exists.
 		/// </summary>
 		public static readonly Configuration Example = new Configuration
 		{
 			PathToCloneDetectionBinary = @"C:\temp\binary.exe",
-			CloneDetectionArgs = "-myarg 2 -bla true"
+			CloneDetectionArgs = "-myarg 2 -bla true",
+			DatabaseType = DatabaseType.SQLiteTemp,
+			DatabaseConnectionString = null
 		};
 	}
 }
