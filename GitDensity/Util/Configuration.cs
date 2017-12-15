259edce75af45c9d529f44c2aa2ee4e78d9ab604
Sebastian Hönel <sehoaa@lnu.se>
2017-12-15 11:09:15 +0100

    Add help for parameters to Configuration

--- GitDensity/Util/Configuration.cs
+++ GitDensity/Util/Configuration.cs
@@ -14,6 +14,15 @@ namespace GitDensity.Util
 		public const String DefaultFileName = "configuration.json";
 
 		/// <summary>
+		/// When writing out a new example, we include the helptext as first property
+		/// with the order -2 (because the other properties have an implicit order of
+		/// -1).
+		/// <see cref="https://stackoverflow.com/a/14035431"/>
+		/// </summary>
+		[JsonProperty(Required = Required.Default, PropertyName = "//", Order = -2)]
+		public String Help { get; set; }
+
+		/// <summary>
 		/// The absolute path to the executable for handling clone detection.
 		/// This should not contain any arguments, use the other property
 		/// for that.
@@ -53,6 +62,11 @@ namespace GitDensity.Util
 		/// </summary>
 		public static readonly Configuration Example = new Configuration
 		{
+			Help = $@"This is the Helptext for this configuration. Launch the program with '--help' to get more help on available switches. Most of the properties you may adjust are boolean, numbers or strings. Some properties require a specific value - those will be listed below:
+
+-> List of supported Database-Types: {{ { String.Join(", ", Enum.GetValues(typeof(DatabaseType))
+									.Cast<DatabaseType>().Select(dbt => dbt.ToString())) } }}",
+
 			PathToCloneDetectionBinary = @"C:\temp\binary.exe",
 			CloneDetectionArgs = "-myarg 2 -bla true",
 			DatabaseType = DatabaseType.SQLiteTemp,
