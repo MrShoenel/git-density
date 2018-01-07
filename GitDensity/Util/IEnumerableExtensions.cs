c1e7b7cf0b55c93b348250408ce1ca33ebe33520
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-07 23:07:36 +0100

    Initial commit of String- and IEnumerable-extensions

---
+++ GitDensity/Util/IEnumerableExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GitDensity.Util
+{
+	public static class IEnumerableExtensions
+	{
+		/// <summary>
+		/// Partitions an <see cref="IEnumerable{T}"/> into chunks of same length.
+		/// If not enough elements are available for the last chunk (i.e. th number
+		/// of available elements is lower than the available amount of elements),
+		/// the last chunk will contain fewer elements.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="items"></param>
+		/// <param name="partitionSize"></param>
+		/// <returns></returns>
+		public static IEnumerable<IList<T>> Partition<T>(this IEnumerable<T> items, int partitionSize)
+		{
+			var i = 0;
+			var array = new T[partitionSize];
+			foreach (var item in items)
+			{
+				array[i++] = item;
+
+				if (i == partitionSize)
+				{
+					yield return array.ToList();
+					i = 0;
+				}
+			}
+
+			if (i > 0)
+			{
+				yield return array.Take(i).ToList();
+			}
+		}
+	}
+}
---
+++ GitDensity/Util/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDensity.Util
+{
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Splits a <see cref="string"/> into lines and returns them as string.
+		/// </summary>
+		/// <param name="str"></param>
+		/// <param name="removeEmptyLines"></param>
+		/// <returns></returns>
+		public static IEnumerable<string> GetLines(this string str, bool removeEmptyLines = false)
+		{
+			using (var sr = new StringReader(str))
+			{
+				string line;
+				while ((line = sr.ReadLine()) != null)
+				{
+					if (removeEmptyLines && String.IsNullOrWhiteSpace(line))
+					{
+						continue;
+					}
+					yield return line;
+				}
+			}
+		}
+	}
+}
