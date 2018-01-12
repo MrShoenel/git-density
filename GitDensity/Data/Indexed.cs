b247ebd504b7429849a2f60b83865113a0d7df12
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-12 13:32:06 +0100

    Add convention and attribute for indexed properties

---
+++ GitDensity/Data/Indexed.cs
@@ -0,0 +1,68 @@
+﻿/// ---------------------------------------------------------------------------------
+///
+/// Copyright (c) 2018 Sebastian Hönel [sebastian.honel@lnu.se]
+///
+/// https://github.com/MrShoenel/git-density
+///
+/// This file is part of the project GitDensity. All files in this project,
+/// if not noted otherwise, are licensed under the MIT-license.
+///
+/// ---------------------------------------------------------------------------------
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+/// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+///
+/// ---------------------------------------------------------------------------------
+///
+using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.Instances;
+using System;
+
+namespace GitDensity.Data
+{
+	/// <summary>
+	/// Used to annotate an entity's property so that it becomes indexed.
+	/// This attribute can also be used for properties that shall be unique.
+	/// </summary>
+	public class IndexedAttribute : Attribute
+	{
+		public Boolean Unique { get; set; }
+
+		public String Name { get; set; }
+
+		public IndexedAttribute()
+		{
+		}
+	}
+
+	public class IndexedConvention : AttributePropertyConvention<IndexedAttribute>
+	{
+		protected override void Apply(IndexedAttribute attribute, IPropertyInstance instance)
+		{
+			var name = (attribute.Name ?? instance.Name).ToUpperInvariant();
+
+			if (attribute.Unique)
+			{
+				instance.UniqueKey($"UNQ_{name}");
+			}
+			else
+			{
+				instance.Index($"IDX_{name}");
+			}
+		}
+	}
+}
