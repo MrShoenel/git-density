b96649bf19eb20bd5c63fc8c0402161e502fecd3
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-13 17:01:58 +0100

    Add method CleanUpDatabase() to ProjectEntity to detect, repair, update and - if necessary - delete entities

--- GitDensity/Data/Entities/ProjectEntity.cs
+++ GitDensity/Data/Entities/ProjectEntity.cs
@@ -29,7 +29,12 @@
 /// ---------------------------------------------------------------------------------
 ///
 using FluentNHibernate.Mapping;
+using GitDensity.Util;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Concurrent;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace GitDensity.Data.Entities
 {
@@ -48,8 +53,113 @@ namespace GitDensity.Data.Entities
 		public virtual String Name { get; set; }
 		public virtual ProjectEntityLanguage Language { get; set; }
 		public virtual String CloneUrl { get; set; }
+		public virtual Boolean WasCorrected { get; set; }
 
 		public virtual RepositoryEntity Repository { get; set; }
+
+		private static BaseLogger<ProjectEntity> logger = Program.CreateLogger<ProjectEntity>();
+
+		/// <summary>
+		/// Cleans up <see cref="ProjectEntity"/>s in the database and attempts to probe
+		/// and repair the git clone-URL. Checks and modifies <see cref="ProjectEntity.WasCorrected"/> as the status of an entity. Some entities
+		/// have empty clone-URLs and cannot be fixed; those will be deleted, if required.
+		/// </summary>
+		/// <param name="deleteUselessEntities">If true, will delete such entities,
+		/// that do not have a clone-URL. This method was primarily made to fix broken
+		/// URLs. Entities without a repairable URL or any URL at all were considered
+		/// useless.</param>
+		public static void CleanUpDatabase(bool deleteUselessEntities = false)
+		{
+			using (var tempSess = DataFactory.Instance.OpenSession())
+			{
+				logger.LogInformation("Successfully probed the configured database.");
+
+
+				var ps = tempSess.QueryOver<Data.Entities.ProjectEntity>().Where(p => !p.WasCorrected).Future();
+				var toSave = new ConcurrentBag<ProjectEntity>();
+				var toDelete = new ConcurrentBag<ProjectEntity>();
+
+				Parallel.ForEach(ps, proj =>
+				{
+					if (String.IsNullOrEmpty(proj.CloneUrl))
+					{
+						logger.LogDebug("Entity with ID {0} has an empty clone-URL.", proj.AiId);
+						toDelete.Add(proj);
+						return;
+					}
+
+					String realUrl = null;
+					try
+					{
+						realUrl = proj.CloneUrl.Substring(0, proj.CloneUrl.LastIndexOf('/')) + "/" + proj.Name + ".git";
+					}
+					catch (Exception e)
+					{
+						logger.LogError(e, e.Message);
+					}
+
+					if (realUrl == proj.CloneUrl)
+					{
+						proj.WasCorrected = true;
+						toSave.Add(proj);
+						return;
+					}
+
+					using (var wc = new HttpClient())
+					{
+						var req = new HttpRequestMessage(HttpMethod.Head, realUrl);
+						var task = wc.SendAsync(req);
+						task.Wait();
+						if (task.Result.StatusCode == System.Net.HttpStatusCode.OK)
+						{
+							logger.LogDebug("OK: {0}", realUrl);
+							// We should update the URL:
+							proj.CloneUrl = realUrl;
+							proj.WasCorrected = true;
+							toSave.Add(proj);
+						}
+						else if (task.Result.StatusCode == System.Net.HttpStatusCode.NotFound)
+						{
+							logger.LogError("NOT FOUND: {0}", realUrl);
+							// Usually happens when the repository disappears.
+							toDelete.Add(proj);
+						}
+						else
+						{
+							logger.LogError("ERROR: {0}", realUrl);
+						}
+					}
+				});
+
+
+				if (deleteUselessEntities)
+				{
+					using (var trans = tempSess.BeginTransaction())
+					{
+						foreach (var item in toDelete)
+						{
+							tempSess.Delete(item);
+						}
+						trans.Commit();
+						logger.LogInformation("Cleaned up {0} broken items.", toDelete.Count);
+					}
+				}
+
+
+				foreach (var part in toSave.Partition(200))
+				{
+					using (var trans = tempSess.BeginTransaction())
+					{
+						foreach (var item in part)
+						{
+							tempSess.Update(item);
+						}
+						trans.Commit();
+						logger.LogInformation("Repaired {0} items and stored them successfully.", part.Count);
+					}
+				}
+			}
+		}
 	}
 
 	/// <summary>
@@ -67,8 +177,11 @@ namespace GitDensity.Data.Entities
 				.Index("IDX_LANGUAGE")
 				.CustomType<StringEnumMapper<ProjectEntityLanguage>>();
 			this.Map(x => x.CloneUrl).Column("CLONE_URL");
+			this.Map(x => x.WasCorrected).Column("WAS_CORRECTED");
 
 			this.HasOne<RepositoryEntity>(x => x.Repository).Cascade.Lock();
 		}
 	}
+
+	
 }
