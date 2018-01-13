2a822426cc3cb9dd62bf0d3f942febf3520ed2a9
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-13 17:02:18 +0100

    Improve naming of and referencing in TreeEntryChangesEntity

--- GitDensity/Data/Entities/TreeEntryChangesEntity.cs
+++ GitDensity/Data/Entities/TreeEntryChangesEntity.cs
@@ -1,10 +1,8 @@
 ﻿using FluentNHibernate.Mapping;
+using GitDensity.Util;
 using LibGit2Sharp;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GitDensity.Data.Entities
 {
@@ -20,16 +18,22 @@ namespace GitDensity.Data.Entities
 		public virtual String PathNew { get; set; }
 
 		public virtual CommitPairEntity CommitPair { get; set; }
+
+		public virtual ISet<FileBlockEntity> FileBlocks { get; set; }
 	}
 
 	public class TreeEntryChangesEntityMap : ClassMap<TreeEntryChangesEntity>
 	{
 		public TreeEntryChangesEntityMap()
 		{
+			this.Table(nameof(TreeEntryChangesEntity).ToSimpleUnderscoreCase());
+
 			this.Id(x => x.ID).GeneratedBy.Identity();
 			this.Map(x => x.Status).CustomType<ChangeKind>().Not.Nullable();
 			this.Map(x => x.PathOld).Length(1000);
 			this.Map(x => x.PathNew).Length(1000);
+
+			this.HasMany<FileBlockEntity>(x => x.FileBlocks).Cascade.Lock();
 
 			this.References<CommitPairEntity>(x => x.CommitPair).Not.Nullable();
 		}
