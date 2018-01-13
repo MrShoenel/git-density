ef0866a6404c3f22ce8082925932e1e37ce9bb13
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-13 16:58:02 +0100

    Add new extension-class for DirectoryInfo and method to clear/wipe a directory clean

---
+++ GitDensity/Util/DirectoryInfoExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDensity.Util
+{
+	public static class DirectoryInfoExtensions
+	{
+		/// <summary>
+		/// Clears a <see cref="DirectoryInfo"/>. That means that all files and folders
+		/// in it will be deleted recursively, without the actual directory being deleted.
+		/// </summary>
+		/// <param name="directoryInfo">The director to clear/wipe/empty.</param>
+		/// <returns>The same given <see cref="DirectoryInfo"/>.</returns>
+		public static DirectoryInfo Clear(this DirectoryInfo directoryInfo)
+		{
+			foreach (var file in directoryInfo.GetFiles())
+			{
+				file.Delete();
+			}
+			foreach (var dir in directoryInfo.GetDirectories())
+			{
+				dir.Delete(true);
+			}
+
+			return directoryInfo;
+		}
+	}
+}
