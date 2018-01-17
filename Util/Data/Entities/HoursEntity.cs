f2fe81403bd89be88e9bd3e61fca753ef27b117c
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-17 18:25:24 +0100

    Add newly and fully implemented classes for HoursEntity and SimpleLOC to Util-project; also add the GitDensity.licenseheader file

---
+++ Util/Data/Entities/HoursEntity.cs
@@ -0,0 +1,65 @@
+﻿/// ---------------------------------------------------------------------------------
+///
+/// Copyright (c) 2018 Sebastian Hönel [sebastian.honel@lnu.se]
+///
+/// https://github.com/MrShoenel/git-density
+///
+/// This file is part of the project Util. All files in this project,
+/// if not noted otherwise, are licensed under the MIT-license.
+///
+/// ---------------------------------------------------------------------------------
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+/// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+///
+/// ---------------------------------------------------------------------------------
+///
+using FluentNHibernate.Mapping;
+using System;
+using Util.Extensions;
+
+namespace Util.Data.Entities
+{
+	public class HoursEntity
+	{
+		public virtual UInt32 ID { get; set; }
+
+		public virtual DeveloperEntity Developer { get; set; }
+
+		public virtual CommitEntity CommitSince { get; set; }
+
+		public virtual CommitEntity CommitUntil { get; set; }
+
+		public virtual Double Hours { get; set; }
+	}
+
+	public class HoursEntityMap : ClassMap<HoursEntity>
+	{
+		public HoursEntityMap()
+		{
+			this.Table(nameof(HoursEntity).ToSimpleUnderscoreCase());
+
+			this.Id(x => x.ID).GeneratedBy.Identity();
+
+			this.Map(x => x.Hours).Not.Nullable();
+
+			this.References<DeveloperEntity>(x => x.Developer).Not.Nullable();
+			this.References<CommitEntity>(x => x.CommitSince).Not.Nullable();
+			this.References<CommitEntity>(x => x.CommitUntil).Not.Nullable();
+		}
+	}
+}
---
+++ Util/Metrics/SimpleLOC.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Util.Metrics
+{
+	/// <summary>
+	/// A very simple lines of code metric that keeps track of the gross amount
+	/// and is able to remove simple single- and multi-line comments as well as
+	/// empty lines containing only whitespace.
+	/// </summary>
+	public class SimpleLOC
+	{
+		#region Regexes
+		/// <summary>
+		/// Matches single lines that start with 2 or more forward slashes. Those
+		/// lines may contain whitespace before the slashes.
+		/// </summary>
+		public static readonly Regex MatchSingleLineComment =
+			new Regex(@"^\s*?\/{2,}.*", RegexOptions.Compiled);
+
+		/// <summary>
+		/// Matches multi-line comments beginning with '/*' and ending with '*/'
+		/// that contain any kind of character.
+		/// </summary>
+		public static readonly Regex MatchMultiLineComment =
+			new Regex(@"\/\*(?:(.|[\r\n])*?)\*\/", RegexOptions.Compiled | RegexOptions.Multiline);
+		#endregion
+
+		protected IEnumerable<String> lines;
+
+		/// <summary>
+		/// The gross amount of lines of code. Counts every line (even empty lines)
+		/// within the string.
+		/// </summary>
+		public UInt32 LocGross => this.lazyLocGross.Value;
+
+		/// <summary>
+		/// Lines of code without empty lines, single- and multi-line comments.
+		/// </summary>
+		public UInt32 LocNoComments => this.lazyLocNoComments.Value;
+
+		private Lazy<UInt32> lazyLocGross;
+
+		private Lazy<UInt32> lazyLocNoComments;
+
+		/// <summary>
+		/// Construct <see cref="SimpleLOC"/> based on an enumerable of lines.
+		/// </summary>
+		/// <param name="lines"></param>
+		public SimpleLOC(IEnumerable<String> lines)
+		{
+			this.lines = lines;
+
+			this.lazyLocGross = new Lazy<uint>(() =>
+			{
+				return (UInt32)this.lines.Count();
+			});
+
+			this.lazyLocNoComments = new Lazy<uint>(() =>
+			{
+				return (UInt32)MatchMultiLineComment.Replace(String.Join("\n", this.lines), String.Empty)
+					.Split('\n')
+					.Where(line =>
+						!(String.IsNullOrWhiteSpace(line.Trim()) || MatchSingleLineComment.IsMatch(line)))
+					.Count();
+			});
+		}
+	}
+}
