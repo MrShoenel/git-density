f49a03d047406c724ff7b609dd5aa7b2ee2447fb
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-18 23:36:14 +0100

    Clean up GitDensityAnalysisResult and implement method to persist a full repository-based analysis to database

--- GitDensity/Density/GitDensityAnalysisResult.cs
+++ GitDensity/Density/GitDensityAnalysisResult.cs
@@ -1,23 +1,18 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Util.Data;
 using Util.Data.Entities;
+using Util.Logging;
 
 namespace GitDensity.Density
 {
 	internal class GitDensityAnalysisResult
 	{
-		//[System.Runtime.CompilerServices.IndexerName("CommitPair")]
-		//public TreeChanges this[CommitPair pair]
-		//{
-		//	get
-		//	{
-		//		var f = pair;
-		//		return null;
-		//	}
-		//}
+		private static BaseLogger<GitDensityAnalysisResult> logger = Program.CreateLogger<GitDensityAnalysisResult>();
 
 		public RepositoryEntity Repository { get; protected internal set; }
 
@@ -28,6 +23,91 @@ namespace GitDensity.Density
 
 		public void PersistToDatabase()
 		{
+			var start = DateTime.Now;
+			var numEntities = 0;
+			logger.LogInformation("Starting persisting analysis result to database..");
+
+			using (var session = DataFactory.Instance.OpenStatelessSession())
+			using (var trans = session.BeginTransaction())
+			{
+				session.Insert(this.Repository);
+				numEntities++;
+
+				foreach (var dev in this.Repository.Developers)
+				{
+					session.Insert(dev);
+					numEntities++;
+				}
+
+				foreach (var commit in this.Repository.Commits)
+				{
+					session.Insert(commit);
+					numEntities++;
+				}
+
+				foreach (var hour in this.Repository.Developers.SelectMany(dev => dev.Hours))
+				{
+					session.Insert(hour);
+					numEntities++;
+				}
+
+				foreach (var pair in this.Repository.CommitPairs)
+				{
+					session.Insert(pair);
+					numEntities++;
+				}
+
+				foreach (var tree in this.Repository.CommitPairs.SelectMany(cp => cp.TreeEntryChanges))
+				{
+					session.Insert(tree);
+					numEntities++;
+					session.Insert(tree.TreeEntryChangesMetrics);
+					numEntities++;
+
+					foreach (var fb in tree.FileBlocks)
+					{
+						session.Insert(fb);
+						numEntities++;
+						foreach (var sim in fb.Similarities)
+						{
+							session.Insert(sim);
+							numEntities++;
+						}
+					}
+				}
+
+				logger.LogInformation("Trying to commit transaction..");
+				trans.Commit();
+				logger.LogInformation("Success! Storing the result took {0}", (DateTime.Now - start));
+				logger.LogInformation("The ID of the analyzed Repository has become {0}", this.Repository.ID);
+
+				logger.LogDebug("Stored {0} instances totally, thereof:", numEntities);
+				if (logger.IsEnabled(LogLevel.Debug))
+				{
+					logger.LogDebug(
+						"\n`- Repositories:\t\t1\n" +
+						"`- Developers:\t\t\t{0}\n" +
+						"`- Commits:\t\t\t{1}\n" +
+						"`- Git-Hours:\t\t\t{2}\n" +
+						"`- CommitPairs:\t\t\t{3}\n" +
+						"`- TreeEntryChanges:\t\t{4}\n" +
+						"`- TreeEntryChangesMetrics:\t{5}\n" +
+						"`- FileBlocks:\t\t\t{6}\n" +
+						"`- Similarities:\t\t{7}\n",
+						this.Repository.Developers.Count,
+						this.Repository.Commits.Count,
+						this.Repository.Developers.SelectMany(dev => dev.Hours).Count(),
+						this.Repository.CommitPairs.Count,
+						this.Repository.CommitPairs.SelectMany(cp => cp.TreeEntryChanges).Count(),
+						// same:
+						this.Repository.CommitPairs.SelectMany(cp => cp.TreeEntryChanges).Count(),
+						this.Repository.CommitPairs.SelectMany(cp => cp.TreeEntryChanges)
+							.SelectMany(tec => tec.FileBlocks).Count(),
+						this.Repository.CommitPairs.SelectMany(cp => cp.TreeEntryChanges)
+							.SelectMany(tec => tec.FileBlocks.SelectMany(fb => fb.Similarities)).Count()
+					);
+				}
+			}
 
 		}
 	}
