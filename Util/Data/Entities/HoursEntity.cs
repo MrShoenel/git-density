606240a897a90afd1bbadb624997d855f490f3dc
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-20 00:27:24 +0100

    Entirely refactor the HoursEntity and change its semantics to represent amount of hours worked between commits for a developer; also make it carry the total amount since initial commit

--- Util/Data/Entities/HoursEntity.cs
+++ Util/Data/Entities/HoursEntity.cs
@@ -29,22 +29,54 @@
 /// ---------------------------------------------------------------------------------
 ///
 using FluentNHibernate.Mapping;
+using LibGit2Sharp;
 using System;
 using Util.Extensions;
 
 namespace Util.Data.Entities
 {
+	/// <summary>
+	/// Holds the amount of hours worked within two consecutive <see cref="Commit"/>s
+	/// of a developer and their total amount of hours worked since the initial commit.
+	/// </summary>
 	public class HoursEntity
 	{
 		public virtual UInt32 ID { get; set; }
 
 		public virtual DeveloperEntity Developer { get; set; }
 
+		/// <summary>
+		/// Marks the beginning of the computed span (the older of the two consecutive
+		/// commits of the current developer). This <see cref="CommitEntity"/> will be
+		/// null for the analyzed span where <see cref="CommitUntil"/> points to the
+		/// current developer's initial commit (because there possibly cannot another
+		/// commit before).
+		/// </summary>
 		public virtual CommitEntity CommitSince { get; set; }
 
+		/// <summary>
+		/// Marks the end of the computed span (the younger of the two consecutive
+		/// commits of the current developer).
+		/// </summary>
 		public virtual CommitEntity CommitUntil { get; set; }
 
+		/// <summary>
+		/// The amount of hours worked during the two consecutive <see cref="Commit"/>s.
+		/// </summary>
 		public virtual Double Hours { get; set; }
+
+		/// <summary>
+		/// The amount of hours worked since the developer's initial commit.
+		/// </summary>
+		public virtual Double HoursTotal { get; set; }
+
+		/// <summary>
+		/// Points to the initial <see cref="CommitEntity"/> for the current developer.
+		/// That means, that for all <see cref="HoursEntity"/> for the current developer,
+		/// this property points to the same <see cref="CommitEntity"/> (because it never
+		/// changes).
+		/// </summary>
+		public virtual CommitEntity InitialCommit { get; set; }
 	}
 
 	public class HoursEntityMap : ClassMap<HoursEntity>
@@ -56,10 +88,12 @@ namespace Util.Data.Entities
 			this.Id(x => x.ID).GeneratedBy.Identity();
 
 			this.Map(x => x.Hours).Not.Nullable();
+			this.Map(x => x.HoursTotal).Not.Nullable();
 
 			this.References<DeveloperEntity>(x => x.Developer).Not.Nullable();
-			this.References<CommitEntity>(x => x.CommitSince).Not.Nullable();
+			this.References<CommitEntity>(x => x.CommitSince).Nullable();
 			this.References<CommitEntity>(x => x.CommitUntil).Not.Nullable();
+			this.References<CommitEntity>(x => x.InitialCommit).Not.Nullable();
 		}
 	}
 }
