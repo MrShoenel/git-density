d66cc199cde27368c3fdc996fea1a81ea0eda014
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-20 00:32:11 +0100

    Add initial commit to GitHoursAuthorSpan, complete comments and alter behavior to yield an initial span for when at least one commit is present

--- GitHours/Hours/GitHoursAuthorSpan.cs
+++ GitHours/Hours/GitHoursAuthorSpan.cs
@@ -40,20 +40,47 @@ namespace GitHours.Hours
 {
 	internal class GitHoursAuthorSpan
 	{
+		/// <summary>
+		/// The initial commit for the current developer (not the repository's
+		/// initial commit!). This property is included, because it's often handy
+		/// to access it easily for the current developer and we do the computation
+		/// anyway.
+		/// </summary>
+		[JsonIgnore]
+		public Commit InitialCommit { get; private set; }
+
+		/// <summary>
+		/// This property is null when <see cref="Until"/> points to the first
+		/// <see cref="Commit"/> (because there cannot be any commit before).
+		/// </summary>
 		[JsonIgnore]
 		public Commit Since { get; private set; }
 
-		public String SinceId => this.Since.Sha.Substring(0, 8);
+		/// <summary>
+		/// The <see cref="Since"/>-commit's SHA1 hash (8 characters of it). This
+		/// property is null, if <see cref="Until"/> points to the initial commit.
+		/// </summary>
+		public String SinceId => this.Since?.Sha.Substring(0, 8);
 
+		/// <summary>
+		/// The end of the span (inclusive).
+		/// </summary>
 		[JsonIgnore]
 		public Commit Until { get; private set; }
 
+		/// <summary>
+		/// The SHA1 hash of the commit pointed to by <see cref="Until"/>. 8 characters.
+		/// </summary>
 		public String UntilId => this.Until.Sha.Substring(0, 8);
 
+		/// <summary>
+		/// The amount of hours worked within the span of the two <see cref="Commit"/>s.
+		/// </summary>
 		public Double Hours { get; private set; }
 
-		private GitHoursAuthorSpan(Commit since, Commit until, double hours)
+		private GitHoursAuthorSpan(Commit initial, Commit since, Commit until, double hours)
 		{
+			this.InitialCommit = initial;
 			this.Since = since;
 			this.Until = until;
 			this.Hours = hours;
@@ -63,10 +90,14 @@ namespace GitHours.Hours
 		{
 			var commitsSorted = commitsForDeveloper.OrderBy(commit => commit.Author.When).ToList();
 
-			if (commitsSorted.Count < 2)
+			if (commitsSorted.Count == 0)
 			{
 				yield break; // Cannot return any span
 			}
+
+			// Return a special instance for the first (initial) commit, that does not
+			// have a preceding commit:
+			yield return new GitHoursAuthorSpan(commitsSorted[0], null, commitsSorted[0], 0d);
 
 			var hoursUntilCommit = new Dictionary<Int32, Tuple<Commit, Double>>();
 			for (var take = 2; take <= commitsSorted.Count; take++)
@@ -78,7 +109,8 @@ namespace GitHours.Hours
 			foreach (var kv in hoursUntilCommit)
 			{
 				var hours = kv.Value.Item2 - (hoursUntilCommit.ContainsKey(kv.Key - 1) ?  hoursUntilCommit[kv.Key - 1].Item2 : 0);
-				yield return new GitHoursAuthorSpan(commitsSorted[kv.Key - 2], kv.Value.Item1, Math.Round(hours, 3));
+				yield return new GitHoursAuthorSpan(commitsSorted[0],
+					commitsSorted[kv.Key - 2], kv.Value.Item1, Math.Round(hours, 3));
 			}
 		}
 	}
