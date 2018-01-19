67dc8582a8975e7bf37e65f689652acb3d7fd86b
Sebastian Hönel <sehoaa@lnu.se>
2018-01-19 17:12:37 +0100

    Add new unit-test project GitHoursTests with one test currently to ascertain that the detailed hour-spans sum up to the correct total amount

---
+++ GitHoursTests/GitHoursAuthorSpanTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Util.Extensions;
+
+namespace GitHoursTests
+{
+	[TestClass]
+	public class GitHoursAuthorSpanTests
+	{
+		public static DirectoryInfo SolutionDirectory
+			=> new DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Locati‌​on)).Parent.Parent.Parent;
+
+		[TestMethod]
+		public void TestAggregateHoursStats()
+		{
+			var span = new Util.GitHoursSpan(SolutionDirectory.FullName.OpenRepository(), (String)null, (String)null);
+			var analysis = new GitHours.Hours.GitHours(span);
+			var result = analysis.Analyze(includeHourSpans: true);
+
+			foreach (var stat in result.AuthorStats)
+			{
+				Assert.AreEqual(stat.HoursTotal, stat.HourSpans.Select(hs => hs.Hours).Sum(), 0.0000001d);
+			}
+		}
+	}
+}
---
+++ GitHoursTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitHoursTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GitHoursTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06d963a7-2199-401b-a458-f74cd429ef3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
