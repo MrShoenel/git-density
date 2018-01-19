5e48f559342a2c4f81696984923bf4fd590a0e06
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-19 21:42:54 +0100

    Add new enum Util.ExecutionPolicy and interface ISupportsExecutionPolicy for types that support parallel execution to implement

---
+++ Util/ISupportsExecutionPolicy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util
+{
+	/// <summary>
+	/// An enumeration with supported ways to execute GitDensity and related programs.
+	/// </summary>
+	public enum ExecutionPolicy : Int32
+	{
+		/// <summary>
+		/// Use as many resources as possible in parallel, to accomplish the task quicker.
+		/// </summary>
+		Parallel = 1,
+		/// <summary>
+		/// No parallelism, use a linear model to process in a series. This approach is
+		/// more resource friendly, but slower.
+		/// </summary>
+		Linear = 2
+	}
+
+	/// <summary>
+	/// An interface for types that support parallel- and lines execution.
+	/// </summary>
+	public interface ISupportsExecutionPolicy
+	{
+		/// <summary>
+		/// A property to be set on supporting types.
+		/// </summary>
+		ExecutionPolicy ExecutionPolicy { get; set; }
+	}
+}
