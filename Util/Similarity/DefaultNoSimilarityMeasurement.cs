4f98d2a1812a7f93921f279b16d32de6c8af5d84
Sebastian Hönel <sebastian.honel@lnu.se>
2018-01-21 05:17:55 +0100

    Add new class DefaultNoSimilarityMeasurement that is designated to represent no similarity / entire difference

---
+++ Util/Similarity/DefaultNoSimilarityMeasurement.cs
@@ -0,0 +1,35 @@
+﻿using F23.StringSimilarity.Interfaces;
+
+namespace Util.Similarity
+{
+	/// <summary>
+	/// Class that implements <see cref="INormalizedStringDistance"/> and
+	/// always returns 1 for the distance and 0 for the similarity. Mainly
+	/// used for <see cref="SimilarityMeasurementType.None"/>, i.e. when no
+	/// similarity shall be computed.
+	/// </summary>
+	public class DefaultNoSimilarityMeasurement : INormalizedStringDistance, INormalizedStringSimilarity
+	{
+		/// <summary>
+		/// Always returns 1 and does not do any computations.
+		/// </summary>
+		/// <param name="s1"></param>
+		/// <param name="s2"></param>
+		/// <returns>1</returns>
+		public double Distance(string s1, string s2)
+		{
+			return 1d;
+		}
+
+		/// <summary>
+		/// Always returns 0 and does not do any computations.
+		/// </summary>
+		/// <param name="s1"></param>
+		/// <param name="s2"></param>
+		/// <returns>0</returns>
+		public double Similarity(string s1, string s2)
+		{
+			return 0d;
+		}
+	}
+}
