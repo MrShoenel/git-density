bbdfeb7f505362d1009260dd2991c9e658ae2681
Sebastian Hönel <sebastian.honel@lnu.se>
2018-02-08 16:26:44 +0100

    Add new query-functions for the R-based analysis-project

--- Analysis/script.R
+++ Analysis/script.R
@@ -2,7 +2,8 @@ library(RMySQL)
 library(rjson)
 source("./Settings.R")
 source("./Functions/queries.R")
+source("./Functions/analyses.R")
 
 
-d23 <- getData(function() getQuery_allContributions(smtType = 23))
-summary(d23)
+#d23 <- getData(function() getQuery_allContributions(smtType = 23))
+#summary(d23)
