96d01ab5a39d0a1fe22e03d0d5e99a515ecdd902
Sebastian Hönel <sebastian.honel@lnu.se>
2018-02-08 16:05:37 +0100

    Upgrade StringExtensions::OpenRepository() to support custom repository names and to pull may existing repositories

--- Util/Extensions/StringExtensions.cs
+++ Util/Extensions/StringExtensions.cs
@@ -92,19 +92,42 @@ namespace Util.Extensions
 		/// that can be used to clone the repository to local facilities.</param>
 		/// <param name="tempDirectory">If non-null, will be used as temporary path.
 		/// Otherwise, <see cref="Path.GetTempPath()"/> will be used.</param>
+		/// <param name="useRepoName">A custom name to use for the to-clone repository. If
+		/// not given, then <see cref="Path.GetRandomFileName"/> will be used.</param>
+		/// <param name="pullIfAlreadyExists">If true and the there is a repository in the
+		/// designated target path, then the repository is opened and a pull is performed.</param>
+		/// <exception cref="InvalidOperationException">Thrown if a custom repository-name
+		/// was given, but the resulting target directory including that name would already
+		/// exist.</exception>
 		/// <returns>A <see cref="Repository"/> that wraps a local git-repository.</returns>
-		public static Repository OpenRepository(this String @string, String tempDirectory = null)
+		public static Repository OpenRepository(this String @string, String tempDirectory = null, String useRepoName = null, Boolean pullIfAlreadyExists = false)
 		{
 			if (@string.IsHttpUrl())
 			{
+				useRepoName = useRepoName.IsNullOrEmptyOrWhiteSpace() ?
+					null : useRepoName.ToValidFileName();
+
 				// We have to clone the repo to a temporary directory first.
-				var tempPath = Path.Combine(tempDirectory ?? Path.GetTempPath(), Path.GetRandomFileName());
+				var tempPath = Path.Combine(
+					tempDirectory ?? Path.GetTempPath(), useRepoName ?? Path.GetRandomFileName());
+				if (!useRepoName.IsNullOrEmptyOrWhiteSpace() && Directory.Exists(tempPath))
+				{
+					if (pullIfAlreadyExists)
+					{
+						var repository = new Repository(tempPath);
+						Commands.Pull(repository, repository.Commits.First().Author, new PullOptions());
+						return repository;
+					}
+					throw new InvalidOperationException(
+						$"There is already a repository cloned in path {tempPath}");
+				}
 				Directory.CreateDirectory(tempPath);
 
 				return new Repository(Repository.Clone(@string, tempPath));
 			}
 			else
 			{
+				// The string -supposedly- represents an accessible local path:
 				if (!Directory.Exists(@string))
 				{
 					throw new FileNotFoundException("The directory does not exist.", @string);
