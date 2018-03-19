03e277998e6da86b5d9f589a1e54bbb3f32ed272
Sebastian Hönel <sebastian.honel@lnu.se>
2018-03-19 16:07:33 +0100

    Update project's code from MIT to GPLv3 and delete project-local license header files

--- GitHours/Hours/GitHoursAuthorStat.cs
+++ GitHours/Hours/GitHoursAuthorStat.cs
@@ -5,26 +5,11 @@
 /// https://github.com/MrShoenel/git-density
 ///
 /// This file is part of the project GitHours. All files in this project,
-/// if not noted otherwise, are licensed under the MIT-license.
+/// if not noted otherwise, are licensed under the GPLv3-license. You will
+/// find a copy of this file in the project's root directory.
 ///
-/// ---------------------------------------------------------------------------------
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a
-/// copy of this software and associated documentation files (the "Software"),
-/// to deal in the Software without restriction, including without limitation
-/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-/// and/or sell copies of the Software, and to permit persons to whom the
-/// Software is furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in all
-/// copies or substantial portions of the Software.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-/// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/// Note that the license changed from MIT to GPLv3. In general, the license
+/// from the latest public commit applies.
 ///
 /// ---------------------------------------------------------------------------------
 ///
