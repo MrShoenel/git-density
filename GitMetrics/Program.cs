47fbf356554d50240a84e6e368c1d447e2fdf297
Sebastian Hönel <sebastian.honel@lnu.se>
2018-09-24 10:23:00 +0200

    Add and initialize new project GitMetrics

---
+++ GitMetrics/Program.cs
@@ -0,0 +1,59 @@
+﻿/// ---------------------------------------------------------------------------------
+///
+/// Copyright (c) 2018 Sebastian Hönel [sebastian.honel@lnu.se]
+///
+/// https://github.com/MrShoenel/git-density
+///
+/// This file is part of the project GitMetrics. All files in this project,
+/// if not noted otherwise, are licensed under the GPLv3-license. You will
+/// find a copy of this file in the project's root directory.
+///
+/// Note that the license changed from MIT to GPLv3. In general, the license
+/// from the latest public commit applies.
+///
+/// ---------------------------------------------------------------------------------
+///
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util.Logging;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+
+namespace GitMetrics
+{
+	class Program
+	{
+		/// <summary>
+		/// The current global <see cref="LogLevel"/>.
+		/// </summary>
+		public static LogLevel LogLevel { get; private set; }
+
+		/// <summary>
+		/// Shortcut provider for obtaining equally configured loggers per <see cref="Type"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		public static BaseLogger<T> CreateLogger<T>()
+		{
+			return new ColoredConsoleLogger<T>
+			{
+				LogCurrentScope = true,
+				LogCurrentTime = true,
+				LogCurrentType = true,
+				LogLevel = Program.LogLevel
+			};
+		}
+
+		private static BaseLogger<Program> logger = CreateLogger<Program>();
+
+		/// <summary>
+		/// Main entry point of application git-metrics.
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+		}
+	}
+}
