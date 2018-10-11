2cc4868ce1856ef046ac63f0a01462eeb9b2e2b9
Sebastian Hönel <sehoaa@lnu.se>
2018-10-12 00:51:23 +0200

    Force culture en-US in GitMetrics::Program

--- GitMetrics/Program.cs
+++ GitMetrics/Program.cs
@@ -20,6 +20,7 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
