6f977ec07d31a3ec5236311712cc2a112332fc1e
Sebastian Hönel <sehoaa@lnu.se>
2018-12-06 11:40:53 +0100

    Add interfaces IAnalyzedCommit and IAnalyzer<T> in project GitTools

---
+++ GitTools/Analysis/IAnalyzedCommit.cs
@@ -0,0 +1,34 @@
+﻿/// ---------------------------------------------------------------------------------
+///
+/// Copyright (c) 2018 Sebastian Hönel [sebastian.honel@lnu.se]
+///
+/// https://github.com/MrShoenel/git-density
+///
+/// This file is part of the project GitTools. All files in this project,
+/// if not noted otherwise, are licensed under the GPLv3-license. You will
+/// find a copy of this file in the project's root directory.
+///
+/// Note that the license changed from MIT to GPLv3. In general, the license
+/// from the latest public commit applies.
+///
+/// ---------------------------------------------------------------------------------
+///
+using LibGit2Sharp;
+using LINQtoCSV;
+using System;
+
+namespace GitTools.Analysis
+{
+	/// <summary>
+	/// Base interface for <see cref="Commit"/>s as analyzed by instances
+	/// of <see cref="IAnalyzer{T}"/>.
+	/// </summary>
+	public interface IAnalyzedCommit
+	{
+		/// <summary>
+		/// The commit's SHA1.
+		/// </summary>
+		[CsvColumn]
+		String SHA1 { get; }
+	}
+}
---
+++ GitTools/Analysis/IAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿/// ---------------------------------------------------------------------------------
+///
+/// Copyright (c) 2018 Sebastian Hönel [sebastian.honel@lnu.se]
+///
+/// https://github.com/MrShoenel/git-density
+///
+/// This file is part of the project GitTools. All files in this project,
+/// if not noted otherwise, are licensed under the GPLv3-license. You will
+/// find a copy of this file in the project's root directory.
+///
+/// Note that the license changed from MIT to GPLv3. In general, the license
+/// from the latest public commit applies.
+///
+/// ---------------------------------------------------------------------------------
+///
+using System.Collections.Generic;
+
+namespace GitTools.Analysis
+{
+	/// <summary>
+	/// Interface for all commit-analyzers to implement.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IAnalyzer<out T> where T : IAnalyzedCommit
+	{
+		/// <summary>
+		/// Yields <see cref="IAnalyzedCommit"/> entities once run.
+		/// </summary>
+		/// <returns></returns>
+		IEnumerable<T> AnalyzeCommits();
+	}
+}
