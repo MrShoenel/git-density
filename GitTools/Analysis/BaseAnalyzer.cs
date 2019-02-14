46452ef3480180c671f06740807fcaed86f49e22
Sebastian Hönel <sebastian.honel@lnu.se>
2019-02-14 16:44:41 +0100

    Add new feature to BaseAnalyzer for for obtaining generated labels for all distinct authors and committers, using methods InitializeNominalSignatures() and AuthorAndCommitterNominalForCommit()

--- GitTools/Analysis/BaseAnalyzer.cs
+++ GitTools/Analysis/BaseAnalyzer.cs
@@ -50,6 +50,12 @@ namespace GitTools.Analysis
 		/// </summary>
 		public GitCommitSpan GitCommitSpan { get; protected set; }
 
+		private IReadOnlyDictionary<Signature, DeveloperWithAlternativeNamesAndEmails> authorSignatures;
+
+		private IReadOnlyDictionary<Signature, DeveloperWithAlternativeNamesAndEmails> committerSignatures;
+
+		private IReadOnlyDictionary<DeveloperEntity, String> developerNominals;
+
 		/// <summary>
 		/// Base constructor that initalizes the repo-path and commits-span.
 		/// </summary>
@@ -59,7 +65,68 @@ namespace GitTools.Analysis
 		{
 			this.RepoPathOrUrl = repoPathOrUrl;
 			this.GitCommitSpan = span;
+			this.InitializeNominalSignatures();
 		}
+
+		#region Nominal Signatures
+		/// <summary>
+		/// Will map each <see cref="Signature"/> uniquely to a <see cref="DeveloperEntity"/>.
+		/// Then, for each entity, a unique ID (within the current repository) is assigned. The
+		/// IDs look like Excel columns.
+		/// </summary>
+		private void InitializeNominalSignatures()
+		{
+			this.Logger.LogInformation("Initializing developer identities for repository..");
+
+			this.developerNominals = new Dictionary<DeveloperEntity, String>();
+
+			this.authorSignatures = new ReadOnlyDictionary<Signature, DeveloperWithAlternativeNamesAndEmails>(
+				this.GitCommitSpan.GroupByDeveloperAsSignatures(
+					repository: null, useAuthorAndNotCommitter: true));
+			this.committerSignatures = new ReadOnlyDictionary<Signature, DeveloperWithAlternativeNamesAndEmails>(
+				this.GitCommitSpan.GroupByDeveloperAsSignatures(
+					repository: null, useAuthorAndNotCommitter: false));
+
+			Func<Int32, String> excelColumnFromNumber = num =>
+			{
+				var colLetters = new LinkedList<Char>();
+				while (num > 0)
+				{
+					var currentLetterNum = (num - 1) % 26;
+					var currentLetter = (char)(currentLetterNum + 65);
+					colLetters.AddLast(currentLetter);
+					num = (num - (currentLetterNum + 1)) / 26;
+				}
+				return new string(colLetters.Reverse().ToArray()).ToUpper();
+			};
+
+			var set = new HashSet<DeveloperEntity>(this.authorSignatures.Select(kv => kv.Value)
+				.Concat(this.committerSignatures.Select(kv => kv.Value)));
+
+			this.developerNominals = new ReadOnlyDictionary<DeveloperEntity, String>(
+				set.Select((dev, idx) => new {
+					Dev = dev,
+					Id = idx + 1
+				}).ToDictionary(kv => kv.Dev, kv => excelColumnFromNumber(kv.Id))
+			);
+
+			this.Logger.LogInformation($"Found {String.Format("{0:n0}", this.authorSignatures.Count + this.committerSignatures.Count)} signatures and mapped them to {String.Format("{0:n0}", this.developerNominals.Count)} distinct developer identities.");
+		}
+
+		/// <summary>
+		/// Returns a unique nominal ID for a <see cref="Commit.Author"/> and
+		/// <see cref="Commit.Committer"/> (<see cref="InitializeNominalSignatures"/>).
+		/// </summary>
+		/// <param name="commit"></param>
+		/// <param name="authorNominal"></param>
+		/// <param name="committerNominal"></param>
+		protected void AuthorAndCommitterNominalForCommit(Commit commit, out string authorNominal, out string committerNominal)
+		{
+			// TODO: Refactor this to use the SHA256 and remove obsolete code in InitializeNominalSignatures
+			authorNominal = this.developerNominals[this.authorSignatures[commit.Author]];
+			committerNominal = this.developerNominals[this.committerSignatures[commit.Committer]];
+		}
+		#endregion
 
 		#region ISupportsExecutionPolicy
 		/// <summary>
