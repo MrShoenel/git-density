ea4f1640170856cd0262744bc63c96f76f4d1a8e
Sebastian Hönel <sebastian.honel@lnu.se>
2019-02-21 14:03:35 +0100

    Add some new properties to ExtendedCommitDetails to reflect net-size and net-affected files better; also add auto-properties for Density and AffectedFilesRatioNet

--- GitTools/Analysis/ExtendedAnalyzer/ExtendedCommitDetails.cs
+++ GitTools/Analysis/ExtendedAnalyzer/ExtendedCommitDetails.cs
@@ -58,38 +58,111 @@ namespace GitTools.Analysis.ExtendedAnalyzer
 		public UInt32 NumberOfFilesAdded { get; protected internal set; } = 0u;
 
 		[CsvColumn(FieldIndex = 18)]
-		public UInt32 NumberOfLinesAddedByAddedFilesNoComments { get; protected internal set; } = 0u;
+		public UInt32 NumberOfFilesAddedNet { get; protected internal set; } = 0u;
 
 		[CsvColumn(FieldIndex = 19)]
-		public UInt32 NumberOfLinesAddedByAddedFilesGross { get; protected internal set; } = 0u;
+		public UInt32 NumberOfLinesAddedByAddedFiles { get; protected internal set; } = 0u;
 
 		[CsvColumn(FieldIndex = 20)]
-		public UInt32 NumberOfFilesDeleted { get; protected internal set; } = 0u;
+		public UInt32 NumberOfLinesAddedByAddedFilesNet { get; protected internal set; } = 0u;
 
 		[CsvColumn(FieldIndex = 21)]
-		public UInt32 NumberOfLinesDeletedByDeletedFilesNoComments { get; protected internal set; } = 0u;
+		public UInt32 NumberOfFilesDeleted { get; protected internal set; } = 0u;
 
 		[CsvColumn(FieldIndex = 22)]
-		public UInt32 NumberOfLinesDeletedByDeletedFilesGross { get; protected internal set; } = 0u;
-
+		public UInt32 NumberOfFilesDeletedNet { get; protected internal set; } = 0u;
 
 		[CsvColumn(FieldIndex = 23)]
-		public UInt32 NumberOfFilesModified { get; protected internal set; } = 0u;
+		public UInt32 NumberOfLinesDeletedByDeletedFiles { get; protected internal set; } = 0u;
 
 		[CsvColumn(FieldIndex = 24)]
-		public UInt32 NumberOfFilesRenamed { get; protected internal set; } = 0u;
-	
+		public UInt32 NumberOfLinesDeletedByDeletedFilesNet { get; protected internal set; } = 0u;
+
+
 		[CsvColumn(FieldIndex = 25)]
-		public UInt32 NumberOfLinesAddedByModifiedFiles { get; protected internal set; } = 0u;
+		public UInt32 NumberOfFilesModified { get; protected internal set; } = 0u;
 
 		[CsvColumn(FieldIndex = 26)]
-		public UInt32 NumberOfLinesDeletedByModifiedFiles { get; protected internal set; } = 0u;
+		public UInt32 NumberOfFilesModifiedNet { get; protected internal set; } = 0u;
 
 		[CsvColumn(FieldIndex = 27)]
-		public UInt32 NumberOfLinesAddedByRenamedFiles { get; protected internal set; } = 0u;
+		public UInt32 NumberOfFilesRenamed { get; protected internal set; } = 0u;
 
 		[CsvColumn(FieldIndex = 28)]
+		public UInt32 NumberOfFilesRenamedNet { get; protected internal set; } = 0u;
+
+		[CsvColumn(FieldIndex = 29)]
+		public UInt32 NumberOfLinesAddedByModifiedFiles { get; protected internal set; } = 0u;
+
+		[CsvColumn(FieldIndex = 30)]
+		public UInt32 NumberOfLinesAddedByModifiedFilesNet { get; protected internal set; } = 0u;
+
+		[CsvColumn(FieldIndex = 31)]
+		public UInt32 NumberOfLinesDeletedByModifiedFiles { get; protected internal set; } = 0u;
+
+		[CsvColumn(FieldIndex = 32)]
+		public UInt32 NumberOfLinesDeletedByModifiedFilesNet { get; protected internal set; } = 0u;
+
+		[CsvColumn(FieldIndex = 33)]
+		public UInt32 NumberOfLinesAddedByRenamedFiles { get; protected internal set; } = 0u;
+
+		[CsvColumn(FieldIndex = 34)]
+		public UInt32 NumberOfLinesAddedByRenamedFilesNet { get; protected internal set; } = 0u;
+
+		[CsvColumn(FieldIndex = 35)]
 		public UInt32 NumberOfLinesDeletedByRenamedFiles { get; protected internal set; } = 0u;
+
+		[CsvColumn(FieldIndex = 36)]
+		public UInt32 NumberOfLinesDeletedByRenamedFilesNet { get; protected internal set; } = 0u;
+		#endregion
+
+		#region additional generated fields
+		/// <summary>
+		/// Returns the ratio between all lines added/removed gross and all lines
+		/// added/removed net ([0,1]).
+		/// </summary>
+		[CsvColumn(FieldIndex = 37)]
+		public Double Density
+		{
+			get
+			{
+				var netLines = this.NumberOfLinesAddedByAddedFilesNet
+					+ this.NumberOfLinesAddedByModifiedFilesNet
+					+ this.NumberOfLinesAddedByRenamedFilesNet
+					+ this.NumberOfLinesDeletedByDeletedFilesNet
+					+ this.NumberOfLinesDeletedByModifiedFilesNet
+					+ this.NumberOfLinesDeletedByRenamedFilesNet;
+				var grossLines = this.NumberOfLinesAddedByAddedFiles
+					+ this.NumberOfLinesAddedByModifiedFiles
+					+ this.NumberOfLinesAddedByRenamedFiles
+					+ this.NumberOfLinesDeletedByDeletedFiles
+					+ this.NumberOfLinesDeletedByModifiedFiles
+					+ this.NumberOfLinesDeletedByRenamedFiles;
+
+				return grossLines == 0u ? 0d :
+					Math.Round((double)netLines / (double)grossLines, 5);
+			}
+		}
+
+		/// <summary>
+		/// Returns the ratio of files changed (add/del/mov/mod) in a commit and
+		/// the amount of files affected net (an affected file is one that had
+		/// more than zero lines net added/deleted after).
+		/// </summary>
+		[CsvColumn(FieldIndex = 38)]
+		public Double AffectedFilesRatioNet
+		{
+			get
+			{
+				var numAffectedFiles = this.NumberOfFilesAdded + this.NumberOfFilesDeleted +
+					this.NumberOfFilesModified + this.NumberOfFilesRenamed;
+				var numActualAffectedFiles = this.NumberOfFilesAddedNet + this.NumberOfFilesDeletedNet +
+					this.NumberOfFilesModifiedNet + this.NumberOfFilesRenamedNet;
+
+				return numAffectedFiles == 0u ? 0d :
+					Math.Round((double)numActualAffectedFiles / (double)numAffectedFiles, 5);
+			}
+		}
 		#endregion
 	}
 }
