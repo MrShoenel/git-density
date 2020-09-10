cb3d611a2d8cc86a9403ec60aea9b3f3f479b1cd
Sebastian Hönel <sebastian.honel@lnu.se>
2020-09-10 14:32:12 +0200

    Add keywords to SimpleCommitDetails in GitTools

--- GitTools/Analysis/SimpleAnalyzer/SimpleCommitDetails.cs
+++ GitTools/Analysis/SimpleAnalyzer/SimpleCommitDetails.cs
@@ -18,6 +18,7 @@ using LINQtoCSV;
 using System;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Util.Data.Entities;
 
 namespace GitTools.Analysis
 {
@@ -39,6 +40,11 @@ namespace GitTools.Analysis
 		protected readonly Commit commit;
 
 		/// <summary>
+		/// Keeps a convenient reference.
+		/// </summary>
+		protected readonly CommitKeywordsEntity keywords;
+
+		/// <summary>
 		/// Initializes a new <see cref="SimpleCommitDetails"/> from a given
 		/// <see cref="Repository"/> and <see cref="Commit"/>.
 		/// </summary>
@@ -48,6 +54,7 @@ namespace GitTools.Analysis
 		{
 			this.RepoPathOrUrl = repoPathOrUrl;
 			this.commit = commit;
+			this.keywords = CommitKeywordsEntity.FromCommit(commit);
 
 			var parents = this.commit.Parents.ToList();
 
@@ -96,6 +103,50 @@ namespace GitTools.Analysis
 
 		[CsvColumn(FieldIndex = 16)]
 		public String ParentCommitSHA1s { get; protected internal set; } = String.Empty;
+
+		#region Keywords
+		[CsvColumn(FieldIndex = 39)]
+		public UInt32 KW_add => this.keywords.KW_add;
+		[CsvColumn(FieldIndex = 40)]
+		public UInt32 KW_allow => this.keywords.KW_allow;
+		[CsvColumn(FieldIndex = 41)]
+		public UInt32 KW_bug => this.keywords.KW_bug;
+		[CsvColumn(FieldIndex = 42)]
+		public UInt32 KW_chang => this.keywords.KW_chang;
+		[CsvColumn(FieldIndex = 43)]
+		public UInt32 KW_error => this.keywords.KW_error;
+		[CsvColumn(FieldIndex = 44)]
+		public UInt32 KW_fail => this.keywords.KW_fail;
+		[CsvColumn(FieldIndex = 45)]
+		public UInt32 KW_fix => this.keywords.KW_fix;
+		[CsvColumn(FieldIndex = 46)]
+		public UInt32 KW_implement => this.keywords.KW_implement;
+		[CsvColumn(FieldIndex = 47)]
+		public UInt32 KW_improv => this.keywords.KW_improv;
+		[CsvColumn(FieldIndex = 48)]
+		public UInt32 KW_issu => this.keywords.KW_issu;
+
+		[CsvColumn(FieldIndex = 49)]
+		public UInt32 KW_method => this.keywords.KW_method;
+		[CsvColumn(FieldIndex = 50)]
+		public UInt32 KW_new => this.keywords.KW_new;
+		[CsvColumn(FieldIndex = 51)]
+		public UInt32 KW_npe => this.keywords.KW_npe;
+		[CsvColumn(FieldIndex = 52)]
+		public UInt32 KW_refactor => this.keywords.KW_refactor;
+		[CsvColumn(FieldIndex = 53)]
+		public UInt32 KW_remov => this.keywords.KW_remov;
+		[CsvColumn(FieldIndex = 54)]
+		public UInt32 KW_report => this.keywords.KW_report;
+		[CsvColumn(FieldIndex = 55)]
+		public UInt32 KW_set => this.keywords.KW_set;
+		[CsvColumn(FieldIndex = 56)]
+		public UInt32 KW_support => this.keywords.KW_support;
+		[CsvColumn(FieldIndex = 57)]
+		public UInt32 KW_test => this.keywords.KW_test;
+		[CsvColumn(FieldIndex = 58)]
+		public UInt32 KW_use => this.keywords.KW_use;
+		#endregion
 
 		#region virtual fields
 		/// <summary>
