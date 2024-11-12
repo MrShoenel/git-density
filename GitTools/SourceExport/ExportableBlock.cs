edb7c55d05d69b0075338586840f4b100559b461
Sebastian Hönel <sebastian.honel@lnu.se>
2024-11-12 11:55:22 +0100

    Inherit from ExportableHunk instead, so we inherit the exportable Fields. Also prefix our fields' names

--- GitTools/SourceExport/ExportableBlock.cs
+++ GitTools/SourceExport/ExportableBlock.cs
@@ -1,6 +1,7 @@
 ﻿using GitDensity.Similarity;
 using LINQtoCSV;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace GitTools.SourceExport
 {
     /// <summary>
     /// Represents a contiguous block of one or more lines that were affected in
-    /// a <see cref="exportableHunk"/>. Contiguous refers to the <see cref="TextBlockNature"/>
+    /// a <see cref="ExportableHunk"/>. Contiguous refers to the <see cref="TextBlockNature"/>
     /// of the block. A hunk may be comprised of multiple interleaved blocks of varying nature.
     /// The nature varies between <see cref="TextBlockNature.Context"/> (i.e., an untouched
     /// block of lines) and changed. Note, however, that a changed block consists of 0 or more
@@ -20,9 +21,9 @@ namespace GitTools.SourceExport
     /// it will directly correspond to <see cref="TextBlockNature.Added"/> or <see cref="TextBlockNature.Deleted"/>,
     /// respectively.
     /// </summary>
-    public class ExportableBlock : ExportableEntity
+    public class ExportableBlock : ExportableHunk, IEnumerable<Line>
     {
-        private ExportableHunk exportableHunk;
+        public ExportableHunk ExportableHunk { get; protected set; }
 
         /// <summary>
         /// The encapsulated <see cref="TextBlock"/> that holds <see cref="Line"/>s.
@@ -30,23 +31,16 @@ namespace GitTools.SourceExport
         public TextBlock TextBlock { get; protected set; }
 
         /// <summary>
-        /// The nature is determined by the (aggregated) nature of the contained
-        /// <see cref="TextBlock"/>'s lines.
-        /// </summary>
-        [CsvColumn(FieldIndex = 5)]
-        public TextBlockNature Nature { get; protected set; }
-
-        /// <summary>
         /// Creates a new contiguous exportable block of lines, by taking into account
-        /// its parent <see cref="ExportableHunk"/>.
+        /// its parent <see cref="SourceExport.ExportableHunk"/>.
         /// </summary>
         /// <param name="exportableHunk"></param>
         /// <param name="textBlock"></param>
         /// <param name="blockIdx"></param>
-        public ExportableBlock(ExportableHunk exportableHunk, TextBlock textBlock, uint blockIdx) : base(exportableHunk.ExportCommit, exportableHunk.TreeChange)
+        public ExportableBlock(ExportableHunk exportableHunk, TextBlock textBlock, uint blockIdx) : base(exportableHunk.ExportCommit, exportableHunk.TreeChange, exportableHunk.Hunk, exportableHunk.HunkIdx)
         {
             this.TextBlock = textBlock;
-            this.exportableHunk = exportableHunk;
+            this.ExportableHunk = exportableHunk;
             this.BlockIdx = blockIdx;
 
             // Let's determine the nature of this block.
@@ -57,35 +51,49 @@ namespace GitTools.SourceExport
         }
 
         /// <summary>
-        /// Essentially, this property is inherited from <see cref="ExportableHunk"/> this
-        /// blocks comes from. Therefore, multiple blocks may refer to the same hunk by index.
+        /// The nature is determined by the (aggregated) nature of the contained
+        /// <see cref="TextBlock"/>'s lines.
         /// </summary>
-        [CsvColumn(FieldIndex = 6)]
-        public UInt32 HunkIdx { get => this.exportableHunk.HunkIdx; }
+        [CsvColumn(FieldIndex = 20)]
+        public TextBlockNature Nature { get; protected set; }
 
         /// <summary>
         /// The zero-based index of the block. This is very similar to <see cref="ExportableHunk.HunkIdx"/>,
         /// but on block-level. The order is top to bottom, with the top-most block having an index of zero.
         /// </summary>
-        [CsvColumn(FieldIndex = 7)]
+        [CsvColumn(FieldIndex = 21)]
         public UInt32 BlockIdx { get; protected set; }
 
-        [CsvColumn(FieldIndex = 8)]
-        public String LineNumbersDeleted { get => String.Join(",", this.TextBlock.LinesWithNumber.Where(kv => kv.Value.Type == LineType.Deleted).Select(kv => kv.Key).OrderBy(k => k)); }
+        [CsvColumn(FieldIndex = 22)]
+        public String BlockLineNumbersDeleted { get => String.Join(",", this.TextBlock.LinesWithNumber.Where(kv => kv.Value.Type == LineType.Deleted).Select(kv => kv.Key).OrderBy(k => k)); }
 
-        [CsvColumn(FieldIndex = 9)]
-        public String LineNumbersAdded { get => String.Join(",", this.TextBlock.LinesWithNumber.Where(kv => kv.Value.Type == LineType.Added).Select(kv => kv.Key).OrderBy(k => k)); }
+        [CsvColumn(FieldIndex = 23)]
+        public String BlockLineNumbersAdded { get => String.Join(",", this.TextBlock.LinesWithNumber.Where(kv => kv.Value.Type == LineType.Added).Select(kv => kv.Key).OrderBy(k => k)); }
 
-        [CsvColumn(FieldIndex = 10)]
-        public String LineNumbersUntouched { get => String.Join(",", this.TextBlock.LinesWithNumber.Where(kv => kv.Value.Type == LineType.Untouched).Select(kv => kv.Key).OrderBy(k => k)); }
+        [CsvColumn(FieldIndex = 24)]
+        public String BlockLineNumbersUntouched { get => String.Join(",", this.TextBlock.LinesWithNumber.Where(kv => kv.Value.Type == LineType.Untouched).Select(kv => kv.Key).OrderBy(k => k)); }
 
 
         /// <summary>
-        /// Similar to <see cref="exportableHunk"/>, the content here are the raw lines. Each
+        /// Similar to <see cref="ExportableHunk"/>, the content here are the raw lines. Each
         /// line starts with a character that designates its nature (i.e., space, +, or -). A
         /// block may also contain empty lines (i.e., one or more whitespace characters).
         /// </summary>
         [CsvColumn(FieldIndex = 999)]
         public override String Content { get => this.TextBlock.WholeBlock; }
+
+        /// <summary>
+        /// Allows this block to be enumerated as lines.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<Line> GetEnumerator()
+        {
+            return this.TextBlock.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
     }
 }
