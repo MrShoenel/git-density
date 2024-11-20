d5c3183dddc1a15fd58fc29dd08e8f8bdc6d646a
Sebastian Hönel <sebastian.honel@lnu.se>
2024-11-20 21:15:20 +0100

    Add aggregate statistics properties and virtual method to copy properties from parents

--- GitTools/SourceExport/ExportableFile.cs
+++ GitTools/SourceExport/ExportableFile.cs
@@ -54,6 +54,29 @@ namespace GitTools.SourceExport
         }
 
         /// <summary>
+        /// This method needs to be called in order to properly inherit aggregation statistics
+        /// from parent entities. For example, the file itself does not know how many files a
+        /// commit actually had, so it has to inherit (copy) this from the commit. This method
+        /// should be overridden in sub-classes and also called from there.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns><see cref="ExportableFile"/> (this instance).</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public virtual ExportableEntity CopyAggregateStatisticsFrom(ExportableEntity entity)
+        {
+            var commit = entity as ExportableCommit;
+            if (!(commit is ExportableCommit))
+            {
+                throw new ArgumentException($"Entity must be of type {nameof(ExportableCommit)}.");
+            }
+            this.CommitNumberOfAddedFiles = commit.CommitNumberOfAddedFiles;
+            this.CommitNumberOfDeletedFiles = commit.CommitNumberOfDeletedFiles;
+            this.CommitNumberOfRenamedFiles = commit.CommitNumberOfRenamedFiles;
+            this.CommitNumberOfModifiedFiles = commit.CommitNumberOfModifiedFiles;
+            return this;
+        }
+
+        /// <summary>
         /// The original event that triggered the creation/removal of this entity.
         /// For example, creating a new file or renaming an existing one. We
         /// retain this information because entities such as single lines do not
@@ -73,6 +96,50 @@ namespace GitTools.SourceExport
         [CsvColumn(FieldIndex = 22)]
         [JsonProperty(Order = 22)]
         public String FileName { get => this.TreeChange.Path; }
+
+        /// <summary>
+        /// The line-count can only be determined if this is a full file (i.e.,
+        /// <see cref="FullCode"/> needs to be true). If true, then this file
+        /// consists of a single hunk which makes the file's new number of lines
+        /// equal to the hunk's new number of lines.
+        /// </summary>
+        [CsvColumn(FieldIndex = 23)]
+        [JsonProperty(Order = 23)]
+        public virtual UInt32? FileNewNumberOfLines { get => this.FullCode ? (UInt32?)this.expoHunks.Single().HunkNewNumberOfLines : null; set => throw new InvalidOperationException(); }
+
+        /// <summary>
+        /// The line-count can only be determined if this is a full file (i.e.,
+        /// <see cref="FullCode"/> needs to be true). If true, then this file
+        /// consists of a single hunk which makes the file's old number of lines
+        /// equal to the hunk's old number of lines.
+        [CsvColumn(FieldIndex = 24)]
+        [JsonProperty(Order = 24)]
+        public virtual UInt32? FileOldNumberOfLines { get => this.FullCode ? (UInt32?)this.expoHunks.Single().HunkOldNumberOfLines : null; set => throw new InvalidOperationException(); }
+
+        /// <summary>
+        /// Returns the number of hunks in this file.
+        /// </summary>
+        [CsvColumn(FieldIndex = 25)]
+        [JsonProperty(Order = 25)]
+        public virtual UInt32 FileNumberOfHunks { get => (UInt32)this.expoHunks.Count; set => throw new InvalidOperationException(); }
+
+        #region overrides
+        [CsvColumn(FieldIndex = 14)]
+        [JsonProperty(Order = 14)]
+        public override UInt32 CommitNumberOfAddedFiles { get; set; }
+
+        [CsvColumn(FieldIndex = 15)]
+        [JsonProperty(Order = 15)]
+        public override UInt32 CommitNumberOfDeletedFiles { get; set; }
+
+        [CsvColumn(FieldIndex = 16)]
+        [JsonProperty(Order = 16)]
+        public override UInt32 CommitNumberOfRenamedFiles { get; set; }
+
+        [CsvColumn(FieldIndex = 17)]
+        [JsonProperty(Order = 17)]
+        public override UInt32 CommitNumberOfModifiedFiles { get; set; }
+        #endregion
 
         /// <summary>
         /// Exporting all changes for one file means that we will just concatenate all
