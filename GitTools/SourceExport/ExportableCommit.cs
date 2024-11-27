02df5b962fb2846b1f81836abe3ef08d8f606227
Sebastian Hönel <sebastian.honel@lnu.se>
2024-11-27 15:58:30 +0100

    Add exportable property for export reason and move other properties

--- GitTools/SourceExport/ExportableCommit.cs
+++ GitTools/SourceExport/ExportableCommit.cs
@@ -17,13 +17,14 @@ using Iesi.Collections.Generic;
 using LibGit2Sharp;
 using LINQtoCSV;
 using Newtonsoft.Json;
-using NHibernate.SqlCommand;
+using Newtonsoft.Json.Converters;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json.Serialization;
 using Util.Extensions;
+using JsonConverter = Newtonsoft.Json.JsonConverter;
+using JsonConverterAttribute = Newtonsoft.Json.JsonConverterAttribute;
 using JsonIgnoreAttribute = Newtonsoft.Json.JsonIgnoreAttribute;
 
 
@@ -64,87 +65,94 @@ namespace GitTools.SourceExport
         [JsonProperty(Order = 2)]
         public String SHA1_Parent { get => this.ExportCommitPair.Parent?.ShaShort() ?? "(initial)"; }
 
+        /// <summary>
+        /// Whether the child of this pair marks the beginning of a potential chain.
+        /// </summary>
         [CsvColumn(FieldIndex = 3)]
-        [JsonProperty(Order = 3)]
-        public String Message { get => this.ExportCommitPair.Child.Message.Trim(); }
+        [JsonProperty(Order = 3), JsonConverter(typeof(StringEnumConverter))]
+        public ExportReason ExportReason { get => this.ExportCommitPair.ExportReason; }
 
         [CsvColumn(FieldIndex = 4)]
         [JsonProperty(Order = 4)]
-        public String AuthorName { get => this.ExportCommitPair.Child.Author.Name; }
+        public String Message { get => this.ExportCommitPair.Child.Message.Trim(); }
 
         [CsvColumn(FieldIndex = 5)]
         [JsonProperty(Order = 5)]
-        public String AuthorEmail { get => this.ExportCommitPair.Child.Author.Email; }
+        public String AuthorName { get => this.ExportCommitPair.Child.Author.Name; }
 
         [CsvColumn(FieldIndex = 6)]
         [JsonProperty(Order = 6)]
+        public String AuthorEmail { get => this.ExportCommitPair.Child.Author.Email; }
+
+        [CsvColumn(FieldIndex = 7)]
+        [JsonProperty(Order = 7)]
         public String AuthorTime { get => this.SerializeDateTime(this.AuthorTime_DT); }
 
         [JsonIgnore]
         public DateTime AuthorTime_DT { get => this.ExportCommitPair.Child.Author.When.UtcDateTime; }
 
-        [CsvColumn(FieldIndex = 7)]
-        [JsonProperty(Order = 7)]
-        public String CommitterName { get => this.ExportCommitPair.Child.Committer.Name; }
-
         [CsvColumn(FieldIndex = 8)]
         [JsonProperty(Order = 8)]
-        public String CommitterEmail { get => this.ExportCommitPair.Child.Committer.Email; }
+        public String CommitterName { get => this.ExportCommitPair.Child.Committer.Name; }
 
         [CsvColumn(FieldIndex = 9)]
         [JsonProperty(Order = 9)]
+        public String CommitterEmail { get => this.ExportCommitPair.Child.Committer.Email; }
+
+        [CsvColumn(FieldIndex = 10)]
+        [JsonProperty(Order = 10)]
 
         public String CommitterTime { get => this.SerializeDateTime(this.CommitterTime_DT); }
 
         [JsonIgnore]
         public DateTime CommitterTime_DT { get => this.ExportCommitPair.Child.Committer.When.UtcDateTime; }
 
-        [CsvColumn(FieldIndex = 10)]
-        [JsonProperty(Order = 10)]
-        public Boolean IsInitialCommit { get => this.ExportCommitPair.Child.Parents.Count() == 0; }
-
         [CsvColumn(FieldIndex = 11)]
         [JsonProperty(Order = 11)]
-        public Boolean IsMergeCommit { get => this.ExportCommitPair.Child.Parents.Count() > 1; }
+        public Boolean IsInitialCommit { get => this.ExportCommitPair.Child.Parents.Count() == 0; }
 
         [CsvColumn(FieldIndex = 12)]
         [JsonProperty(Order = 12)]
+        public Boolean IsMergeCommit { get => this.ExportCommitPair.Child.Parents.Count() > 1; }
+
+        [CsvColumn(FieldIndex = 13)]
+        [JsonProperty(Order = 13)]
         public UInt32 NumberOfParentCommits { get => (uint)this.ExportCommitPair.Child.Parents.Count(); }
 
         /// <summary>
         /// A map between child and parent commit time as the <see cref="TimeSpan"/> between
         /// both. The key is the parent's SHA1 and the value is in fractional minutes.
         /// </summary>
-        [CsvColumn(FieldIndex = 13)]
-        [JsonProperty(Order = 13)]
+        [CsvColumn(FieldIndex = 14)]
+        [JsonProperty(Order = 14)]
         public Double? DaysSinceParentCommit { get => this.ExportCommitPair.Parent is Commit ? (this.ExportCommitPair.Child.Committer.When.UtcDateTime - this.ExportCommitPair.Parent.Committer.When.UtcDateTime).TotalDays : (Double?)null; }
 
         /// <summary>
         /// Returns the number files added in this commit.
         /// </summary>
-        [CsvColumn(FieldIndex = 14)]
-        [JsonProperty(Order = 14)]
+        [CsvColumn(FieldIndex = 15)]
+        [JsonProperty(Order = 15)]
         public virtual UInt32 CommitNumberOfAddedFiles { get => (UInt32)this.expoFiles.Where(f => f.TreeChangeIntent == ChangeKind.Added).Count(); set => throw new NotImplementedException(); }
 
         /// <summary>
         /// Returns the number of files deleted in this commit.
         /// </summary>
-        [CsvColumn(FieldIndex = 15)]
-        [JsonProperty(Order = 15)]
+        [CsvColumn(FieldIndex = 16)]
+        [JsonProperty(Order = 16)]
         public virtual UInt32 CommitNumberOfDeletedFiles { get => (UInt32)this.expoFiles.Where(f => f.TreeChangeIntent == ChangeKind.Deleted).Count(); set => throw new NotImplementedException(); }
 
         /// <summary>
         /// Returns the number of files renamed in this commit.
         /// </summary>
-        [CsvColumn(FieldIndex = 16)]
-        [JsonProperty(Order = 16)]
+        [CsvColumn(FieldIndex = 17)]
+        [JsonProperty(Order = 17)]
         public virtual UInt32 CommitNumberOfRenamedFiles { get => (UInt32)this.expoFiles.Where(f => f.TreeChangeIntent == ChangeKind.Renamed).Count(); set => throw new NotImplementedException(); }
 
         /// <summary>
         /// Returns the number of files modified in this commit.
         /// </summary>
-        [CsvColumn(FieldIndex = 17)]
-        [JsonProperty(Order = 17)]
+        [CsvColumn(FieldIndex = 18)]
+        [JsonProperty(Order = 18)]
         public virtual UInt32 CommitNumberOfModifiedFiles { get => (UInt32)this.expoFiles.Where(f => f.TreeChangeIntent == ChangeKind.Modified).Count(); set => throw new NotImplementedException(); }
 
 
