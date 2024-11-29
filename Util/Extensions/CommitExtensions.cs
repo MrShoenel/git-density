5a7b849501f5bf69d7f1ab6b902682d719b36b64
Sebastian Hönel <sebastian.honel@lnu.se>
2024-11-29 16:44:32 +0100

    Refactor and simplify (and fix potential endless recursion in) method ParentGenerations

--- Util/Extensions/CommitExtensions.cs
+++ Util/Extensions/CommitExtensions.cs
@@ -53,9 +53,13 @@ namespace Util.Extensions
         /// </summary>
         /// <param name="commit"></param>
         /// <param name="numGenerations"></param>
-        /// <exception cref="Exception">If parents for a parent-less commit were requested.</exception>
+        /// <param name="primaryCommit"></param>
+        /// <param name="originalNum"></param>
+        /// <param name="allowIncompleteChains"></param>
+        /// <exception cref="Exception">If parents for a parent-less commit were requested and
+        /// allowIncompleteChains=false.</exception>
         /// <returns></returns>
-        public static ISet<Commit> ParentGenerations(this Commit commit, UInt32 numGenerations, Commit primaryCommit = null, UInt32? originalNum = null)
+        public static ISet<Commit> ParentGenerations(this Commit commit, UInt32 numGenerations, Commit primaryCommit = null, UInt32? originalNum = null, bool allowIncompleteChains = false)
         {
             var usePrimary = primaryCommit ?? commit;
             var useOrgNum = originalNum ?? numGenerations;
@@ -69,23 +73,23 @@ namespace Util.Extensions
             var parents = commit.Parents.ToList();
             if (parents.Count == 0)
             {
-                throw new Exception($"Cannot export {useOrgNum} parent generations. Commit with ID {usePrimary.ShaShort()} only has {useOrgNum - numGenerations} parent generation(s).");
+                var msg = $"Cannot export {useOrgNum} parent generations. Commit with ID {usePrimary.ShaShort()} only has {useOrgNum - numGenerations} parent generation(s) along the chain that ends with commit {commit.ShaShort()}.";
+                if (allowIncompleteChains)
+                {
+                    logger.LogCritical(msg);
+                    return results;
+                }
+                throw new Exception(msg + " You may use the option --allow-incomplete-chains to enable too-short the export of too-short chains.");
             }
+
+            results.AddAll(parents);
 
             // Let's decrease it. Since it's an UInt32, it was > 0 before (no overflow here).
             numGenerations--;
-
-            foreach (var parent in parents)
+            if (numGenerations > 0)
             {
-                results.Add(parent);
-
-                if (numGenerations > 0)
-                {
-                    foreach (var parent_parent in parent.ParentGenerations(numGenerations: numGenerations, primaryCommit: usePrimary, originalNum: useOrgNum))
-                    {
-                        results.AddAll(parent.ParentGenerations(numGenerations: numGenerations, primaryCommit: usePrimary, originalNum: useOrgNum));
-                    }
-                }
+                results.AddAll(parents.SelectMany(parent =>
+                    parent.ParentGenerations(numGenerations: numGenerations, primaryCommit: usePrimary, originalNum: useOrgNum, allowIncompleteChains: allowIncompleteChains)));
             }
 
             return results;
