69fb86952969f10fbde9e4a9b61c5117bb1a5b9a
Sebastian Hönel <sebastian.honel@lnu.se>
2024-12-04 19:09:18 +0100

    Remove old Analysis project

--- Analysis/script.R
+++
@@ -1,9 +0,0 @@
-library(RMySQL)
-library(rjson)
-source("./Settings.R")
-source("./Functions/queries.R")
-source("./Functions/analyses.R")
-
-
-#d23 <- getData(function() getQuery_allContributions(smtType = 23))
-#summary(d23)
